a911d498b5b24d350d0b29a3723d3fe18b85d22d
Rishi Aluri <rishialuri@gmail.com>
2026-01-18 18:53:15 +0000

    Module 1 code + uv

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from data-engg-zoomcamp!")
+
+
+if __name__ == "__main__":
+    main()
--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -8,3 +8,5 @@ print("Hello Pipe!")
 
 df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
 print(f"{df.head()}")
+
+df.to_parquet(f"output_{month}.parquet")
